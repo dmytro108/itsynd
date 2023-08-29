12753012e6a3248ab82114df9b1d0a3f8d6464d8
Dmitry Fedunets <dmitry.fedunets@gmail.com>
2023-08-29 01:52:33 +0000

    fix az

--- week1/task3/main.tf
+++ week1/task3/main.tf
@@ -7,7 +7,7 @@ module "vpc" {
   name = "my-vpc-${count.index}"
   cidr = "10.${count.index}.0.0/16"
 
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
   private_subnets = ["10.0.1.0/24"]
   #public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 

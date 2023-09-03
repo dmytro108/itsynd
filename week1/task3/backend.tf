9d7e5e08fab5fbfcb66c5988d953a803c4a33c9c
Dmitry Fedunets <dmitry.fedunets@gmail.com>
2023-09-03 22:39:47 +0000

    S3 backend

--- week1/task3/backend.tf
+++ week1/task3/backend.tf
@@ -1,13 +1,15 @@
 terraform {
-  /*   backend "s3" {
+  backend "s3" {
     bucket         = "its-week1-task3-00"
     key            = "terraform.tfstate"
-    region         = var.rgion
+    region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "terraform-lock"
-  }
- */
-  backend "local" {
-
+    dynamodb_table = "terraform_locks"
   }
 }
+/*
+aws dynamodb create-table --table-name terraform_locks \
+                          --attribute-definitions AttributeName=LockID,AttributeType=S \
+                          --key-schema AttributeName=LockID,KeyType=HASH \
+                          --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5
+*/

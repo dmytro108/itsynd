f3934ef9797dbd3fa0da10e5fed77134ea30190d
Dmitry Fedunets <dmitry.fedunets@gmail.com>
2023-09-20 12:30:24 +0100

    project templates

---
+++ .fttemplates/task/[FTName]/docs/header.md
@@ -0,0 +1,2 @@
+# IT Syndicate Boot Camp
+## Week . Task
---
+++ .fttemplates/terraform/backend.tf
@@ -0,0 +1,17 @@
+terraform {
+  backend "s3" {
+    bucket         = "its-week3-task1-01"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-locks-its-week3-task1-01"
+  }
+}
+/*
+aws s3  mb --region "us-east-1" s3://its-week3-task1-01
+aws dynamodb create-table --table-name terraform-locks-its-week3-task1-01 \
+                          --attribute-definitions AttributeName=LockID,AttributeType=S \
+                          --key-schema AttributeName=LockID,KeyType=HASH \
+                          --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 \
+                          --region "us-east-1"
+*/

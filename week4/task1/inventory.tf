e71717a0e92ba5eeeb3309c2917c5a0ce52181b4
Dmitry Fedunets <dmitry.fedunets@gmail.com>
2023-09-23 03:34:51 +0100

    generating Ansible inventory, refactoring

--- week4/task1/inventory.tf
+++ week4/task1/inventory.tf
@@ -1,10 +1,10 @@
-resource "local_file" "AnsibleInventory" {
+resource "local_file" "ansible_inventory" {
   content = templatefile("${var.inventory_template}",
     {
       app_servers = aws_instance.app_servers[*].private_ip,
-      db_servers = [aws_instance.db.private_ip]
+      db_servers  = [aws_instance.db.private_ip]
     }
   )
 
-  filename = "${var.inventory_file}"
+  filename = var.inventory_file
 }

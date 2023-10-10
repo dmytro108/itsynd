825cb55aea6afd00e22ef4ad16374a41316b09de
Dmitry Fedunets <dmitry.fedunets@gmail.com>
2023-10-10 10:14:03 +0000

    refactoring

--- aws_base_infrastructure/instance_connect.tf
+++ aws_base_infrastructure/instance_connect.tf
@@ -1,7 +1,9 @@
 # Coonect to the hosts incide the VPC:
-# > aws ec2-instance-connect open-tunnel \
-#        --instance-id <instance_id> \
-#        --local-port <local_port> &
+/*
+ > aws ec2-instance-connect open-tunnel \
+        --instance-id <instance_id> \
+        --local-port <local_port> &
+*/
 # SSh connection:
 # > ssh  ec2-user@localhost -p <local_port>
 #
@@ -9,7 +11,6 @@ resource "aws_ec2_instance_connect_endpoint" "tunnel" {
   subnet_id          = module.vpc.private_subnets[0]
   security_group_ids = [module.sg_tunnel.security_group_id]
 }
-
 
 # ******************* Bastion SG
 module "sg_tunnel" {
